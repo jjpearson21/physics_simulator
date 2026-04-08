dbf1986aec35608262c2e839f40142cfc5db601e
Jared Pearson <jjp102134@gmail.com>
2026-04-07 20:35:07 -0500

    Add second ball along with collision logic

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,40 @@ float last_frame = 0.0f;
 
 // constants
 const float PI = 3.14159265359f;
+const float PARTICLE_RADIUS = 0.2f;
+
+// ===== Structs =====
+struct Particle{
+    float radius;
+    unsigned int segmants = 100;
+
+    // Position
+    float pos_x;
+    float pos_y;
+
+    // Velocity Data
+    float velocity_x;
+    float velocity_y;
+
+    Particle(float x, float y, float r)
+    {
+        radius = r;
+        pos_x = x;
+        pos_y = y;
+
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_real_distribution<float> dist(0.0f, 2.0f * PI);
+
+        float angle = dist(gen);
+        float dir_x = cos(angle);
+        float dir_y = sin(angle);
+        float speed = 0.5f;
+
+        velocity_x = dir_x * speed;
+        velocity_y = dir_y * speed;
+    }
+};
 
 // ===== Functions For Running Program =====
 
@@ -125,20 +159,6 @@ vector<glm::vec2> create_circle(float radius, int segments, float aspect)
 {
     vector<glm::vec2> vertices;
 
-    float radius_x;
-    float radius_y;
-
-    if (aspect > 1.0f)
-    {
-        radius_x = radius / aspect;
-        radius_y = radius;
-    }
-    else
-    {
-        radius_x = radius;
-        radius_y = radius * aspect;
-    }
-
     // Center at origin
     vertices.push_back(glm::vec2(0.0f, 0.0f));
 
@@ -146,8 +166,8 @@ vector<glm::vec2> create_circle(float radius, int segments, float aspect)
     {
         float angle = 2.0f * PI * i / segments;
 
-        float x = radius_x * cos(angle);
-        float y = radius_y * sin(angle);
+        float x = radius * cos(angle);
+        float y = radius * sin(angle);
 
         vertices.push_back(glm::vec2(x, y));
     }
@@ -155,8 +175,70 @@ vector<glm::vec2> create_circle(float radius, int segments, float aspect)
     return vertices;
 }
 
-// ===== MAIN =====
+// Collision Functions
+bool check_collision(Particle &p1, Particle &p2)
+{
+    float dx = p1.pos_x - p2.pos_x;
+    float dy = p1.pos_y - p2.pos_y;
 
+    float distance = sqrt((dx*dx) + (dy*dy));
+
+    return distance <= (p1.radius + p2.radius);
+}
+
+void collision_response(Particle &p1, Particle &p2)
+{
+    // Get direction between centers
+    float dx = p2.pos_x - p1.pos_x;
+    float dy = p2.pos_y - p1.pos_y;
+
+    // Get distance between centers
+    float distance = sqrt((dx * dx) + (dy * dy));
+    if(distance == 0)
+    {
+        return;
+    }
+
+    // Get the collision normal (the line along which the collision happens)
+    float norm_x = dx / distance;
+    float norm_y = dy / distance;
+
+    // Overlap
+    float overlap = (p1.radius + p2.radius) - distance;
+    if(overlap > 0)
+    {
+        p1.pos_x = p1.pos_x - ((overlap / 2) * norm_x);
+        p1.pos_y = p1.pos_y - ((overlap / 2) * norm_y);
+        p2.pos_x = p2.pos_x + ((overlap / 2) * norm_x);
+        p2.pos_y = p2.pos_y + ((overlap / 2) * norm_y);
+    }
+
+    // Relative velocity
+    float rel_velocity_x = p2.velocity_x - p1.velocity_x;
+    float rel_velocity_y = p2.velocity_y - p1.velocity_y;
+
+    // Velocity along normal
+    glm::vec2 rel_velocity = glm::vec2(rel_velocity_x, rel_velocity_y);
+    glm::vec2 norm_position = glm::vec2(norm_x, norm_y);
+    float velo_along_norm = glm::dot(rel_velocity, norm_position);
+
+    // Check if moving toward or apart
+    if (velo_along_norm > 0)
+    {
+        return;
+    }
+
+    // Correct velocity
+    float velo_correct_x = -velo_along_norm * norm_x;
+    float velo_correct_y = -velo_along_norm * norm_y;
+
+    p1.velocity_x -= velo_correct_x;
+    p1.velocity_y -= velo_correct_y;
+    p2.velocity_x += velo_correct_x;
+    p2.velocity_y += velo_correct_y;
+}
+
+// ===== MAIN =====
 int main(void)
 {
     // Initialize glfw
@@ -176,48 +258,16 @@ int main(void)
     Shader mainShader("shaders/v_shader.txt", "shaders/f_shader.txt");
 
     // --- DATA ---
+    Particle p1(-0.5f, 0.0f, PARTICLE_RADIUS);
+    Particle p2(0.5f, 0.0f, PARTICLE_RADIUS);
+
     // Aspect Ratio
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     float aspect = (float)width / (float)height;
 
-    // Circle Data
-    float radius = 0.2f;
-    unsigned int segmants = 100;
-
-    // Actual extents used for collision
-    float radius_x;
-    float radius_y;
-
-    if (aspect > 1.0f)
-    {
-        radius_x = radius / aspect;
-        radius_y = radius;
-    }
-    else
-    {
-        radius_x = radius;
-        radius_y = radius * aspect;
-    }
-
-    // Position Data
-    float circle_pos_x = 0.0f;
-    float circle_pos_y = 0.0f;
-
-    // Velocity Data
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<float> dist(0.0f, 2.0f * PI);
-
-    float angle = dist(gen);
-    float dir_x = cos(angle);
-    float dir_y = sin(angle);
-    float speed = 0.5f;
-    float velocity_x = dir_x * speed;
-    float velocity_y = dir_y * speed;
-
     // Vertex Data
-    vector<glm::vec2> circle = create_circle(radius, segmants, aspect);
+    vector<glm::vec2> particleMesh = create_circle(p1.radius, p1.segmants, aspect);
 
     // --- CONFIGURE CUBE VAO (AND VBO) ---
     unsigned int VBO, VAO;
@@ -227,7 +277,7 @@ int main(void)
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, circle.size() * sizeof(glm::vec2), circle.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, particleMesh.size() * sizeof(glm::vec2), particleMesh.data(), GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (void *)0);
     glEnableVertexAttribArray(0);
@@ -247,47 +297,97 @@ int main(void)
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // --- ASPECT RATIO ---
+        glfwGetFramebufferSize(window, &width, &height);
+        aspect = (float)width / (float)height;
+
         // --- MOVEMENT ---
         // Update circle position
-        circle_pos_x = circle_pos_x + velocity_x * delta_time;
-        circle_pos_y = circle_pos_y + velocity_y * delta_time;
+        p1.pos_x = p1.pos_x + p1.velocity_x * delta_time;
+        p1.pos_y = p1.pos_y + p1.velocity_y * delta_time;
+        p2.pos_x = p2.pos_x + p2.velocity_x * delta_time;
+        p2.pos_y = p2.pos_y + p2.velocity_y * delta_time;
 
-        // X boundaries
-        if (circle_pos_x + radius_x >= 1.0f)
+        // p1 X boundaries
+        if (p1.pos_x + PARTICLE_RADIUS >= aspect)
         {
-            circle_pos_x = 1.0f - radius_x;
-            velocity_x *= -1.0f;
+            p1.pos_x = aspect - PARTICLE_RADIUS;
+            p1.velocity_x *= -1.0f;
         }
-        else if (circle_pos_x - radius_x <= -1.0f)
+        else if (p1.pos_x - PARTICLE_RADIUS <= -aspect)
         {
-            circle_pos_x = -1.0f + radius_x;
-            velocity_x *= -1.0f;
-        }
-
-        // Y boundaries
-        if (circle_pos_y + radius_y >= 1.0f)
-        {
-            circle_pos_y = 1.0f - radius_y;
-            velocity_y *= -1.0f;
-        }
-        else if (circle_pos_y - radius_y <= -1.0f)
-        {
-            circle_pos_y = -1.0f + radius_y;
-            velocity_y *= -1.0f;
+            p1.pos_x = -aspect + PARTICLE_RADIUS;
+            p1.velocity_x *= -1.0f;
         }
 
-        // --- TRANSFORMATIONS ---
-        glm::mat4 transform = glm::mat4(1.0f); // initialize matrix to identity matrix first
-        transform = glm::translate(transform, glm::vec3(circle_pos_x, circle_pos_y, 0.0f));
+        // p1 Y boundaries
+        if (p1.pos_y + PARTICLE_RADIUS >= 1.0f)
+        {
+            p1.pos_y = 1.0f - PARTICLE_RADIUS;
+            p1.velocity_y *= -1.0f;
+        }
+        else if (p1.pos_y - PARTICLE_RADIUS <= -1.0f)
+        {
+            p1.pos_y = -1.0f + PARTICLE_RADIUS;
+            p1.velocity_y *= -1.0f;
+        }
 
-        // --- SHADER STUFF ---
+        // p2 X boundaries
+        if (p2.pos_x + PARTICLE_RADIUS >= aspect)
+        {
+            p2.pos_x = aspect - PARTICLE_RADIUS;
+            p2.velocity_x *= -1.0f;
+        }
+        else if (p2.pos_x - PARTICLE_RADIUS <= -aspect)
+        {
+            p2.pos_x = -aspect + PARTICLE_RADIUS;
+            p2.velocity_x *= -1.0f;
+        }
+
+        // p1 Y boundaries
+        if (p2.pos_y + PARTICLE_RADIUS >= 1.0f)
+        {
+            p2.pos_y = 1.0f - PARTICLE_RADIUS;
+            p2.velocity_y *= -1.0f;
+        }
+        else if (p2.pos_y - PARTICLE_RADIUS <= -1.0f)
+        {
+            p2.pos_y = -1.0f + PARTICLE_RADIUS;
+            p2.velocity_y *= -1.0f;
+        }
+
+        // Collisions
+        if(check_collision(p1, p2) == true)
+        {
+            collision_response(p1, p2);
+        }
+
+        // --- ORTHOGRAPHIC PROJECT MATRIX ---
+        glm::mat4 projection = glm::mat4(1.0f); // initialize matrix to identity matrix first
+        projection = glm::ortho(-aspect, aspect, -1.0f, 1.0f, -1.0f, 1.0f);
+
         mainShader.use();
+        unsigned int projectionLoc = glGetUniformLocation(mainShader.ID, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        // --- FIRST PARTICLE ---
+        glm::mat4 transform = glm::mat4(1.0f); // initialize matrix to identity matrix first
+        transform = glm::translate(transform, glm::vec3(p1.pos_x, p1.pos_y, 0.0f));
+
         unsigned int transformLoc = glGetUniformLocation(mainShader.ID, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
-        // --- DRAW CIRCLE ---
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, circle.size());
+        glDrawArrays(GL_TRIANGLE_FAN, 0, particleMesh.size());
+
+        // --- SECOND PARTICLE ---
+        transform = glm::mat4(1.0f);
+        transform = glm::translate(transform, glm::vec3(p2.pos_x, p2.pos_y, 0.0f));
+
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, particleMesh.size());
 
         // --- SWAP BUFFERS AND POLL IO EVENTS ---
         glfwSwapBuffers(window);
